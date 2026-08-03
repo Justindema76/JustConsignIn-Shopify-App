73d14b8171b9d0b22c5d5a6ed94f169382940ce1
Justin DeMatteis <160768253+Justindema76@users.noreply.github.com>
2026-08-03 19:12:28 -0400

    Use proper product labels in Tier 1 items table

--- justconsignin/app/routes/app._index.jsx
+++ justconsignin/app/routes/app._index.jsx
@@ -1,5 +1,10 @@
 import TierOneConsignmentApp from '../tier1_consignment_app';
+import TierOneProductLabels from '../tier1_product_labels';
 
 export default function AppIndex() {
-  return <TierOneConsignmentApp />;
+  return (
+    <TierOneProductLabels>
+      <TierOneConsignmentApp />
+    </TierOneProductLabels>
+  );
 }
