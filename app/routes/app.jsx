75373617ba2863e5139ce284cbd22ee7d160efe1
Justin DeMatteis <justindematteis-imac@mac.home>
2026-08-30 00:16:22 -0400

    updated routes2

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -69,11 +69,6 @@ export default function App() {
 
   return (
     <AppProvider embedded apiKey={apiKey}>
-      <s-app-nav>
-        <s-link href="/app/plans">Upgrade Plan</s-link>
-        <s-link href="/app/settings">Settings</s-link>
-      </s-app-nav>
-
       <Outlet context={{ theme, setTheme }} />
 
       <style>{`
